faf9a4d89874ef44f55f8d5a63e309fdfde06873
James Giosmas <giosmasj@gmail.com>
2020-05-05 15:12:40 -0600

    created cards

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+
+
 fetch("http://localhost:3000/jobs")
     .then(response => response.json())
     .then(jobs => {
 		jobs.forEach(job => {
+			const card = document.createElement("div")
+			card.setAttribute("class", "card")
+			console.log(card.class)
 			const jobTitle = document.createElement("h2")
 			const jobLocation = document.createElement("h3")
             jobTitle.innerHTML = `<a href=showJob.html?id=${job.id}>${job.title}</a>`
             jobLocation.innerText = job.location
-			document.body.append(jobTitle, jobLocation)
+			card.append(jobTitle, jobLocation)
+			document.body.append(card)
 		})
 	})
